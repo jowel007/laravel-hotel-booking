7f9dcd3e515cc21e1960eb565976c0dfc63cd948
jowel007 <abuhanifjowel1@gmail.com>
2024-12-18 23:34:59 +0600

    room update facility table data update - I

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -9,6 +9,7 @@ use App\Models\Room;
 use App\Models\Facility;
 use Illuminate\Support\Facades\Auth;
 use Intervention\Image\Facades\Image;
+use PHPUnit\Framework\Constraint\Count;
 
 class RoomController extends Controller
 {
@@ -47,10 +48,26 @@ class RoomController extends Controller
         $room->image = $filename;
         $room->save();
 
-      
 
+        if ($request->facility_name == NULL) {
 
+            $notification = array(
+                'message' => 'Sorry! Not Any Basic Facility Selected',
+                'alert-type' => 'error'
+            );
 
+            return redirect()->back()->with($notification);
+        } else {
+            Facility::where('room_id',$id)->delete();
+            $facilities = Count($request->facility_name);
+
+            for ($i=0; $i < $facilities; $i++) {
+                $fcount = new Facility();
+                $fcount->room_id = $room->id;
+                $fcount->facility_name = $request->facility_name[$i];
+                $fcount->save();
+            }
+        }
 
     }
 }
