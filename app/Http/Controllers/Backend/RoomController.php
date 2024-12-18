7f8e4007abc6fb353c0bdfa0bae8e2c1c72dd1bc
jowel007 <abuhanifjowel1@gmail.com>
2024-12-18 23:05:32 +0600

    room edit page added facility design using js - 7

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\Room;
+use App\Models\Facility;
 use Illuminate\Support\Facades\Auth;
 use Intervention\Image\Facades\Image;
 
 class RoomController extends Controller
 {
     public function EditRoom($id){
+        $basic_facility = Facility::where('room_id', $id)->get();
         $editData = Room::find($id);
-        return view('backend.allroom.rooms.edit_room',compact('editData'));
+        return view('backend.allroom.rooms.edit_room',compact('editData','basic_facility'));
     }
 }
